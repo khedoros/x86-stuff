c59d2179d3b65bf77f61aeb80bc7aacd69552496
khedoros <khedoros@gmail.com>
2025-09-09 23:48:00 -0700

    Opcodes, op notes

--- cpu.cpp
+++ cpu.cpp
@@ -1,4 +1,6 @@
+#include<array>
 #include<iostream>
+#include <sys/types.h>
 
 #include "cpu.h"
 #include "util.h"
@@ -9,16 +11,41 @@ uint64_t cpu::step() {
     uint8_t op = mem[getOffset(cs, ip)];
     uint8_t op1 = mem[getOffset(cs, ip + 1)];
     uint8_t op2 = mem[getOffset(cs, ip + 2)];
+
     std::cout << std::hex << cs << ":" << ip << "\t";
-    switch(op) {
-        case 0xba: // mov $0x1234, %dx
-            dx = op2 << 8 | op1;
-            std::cout<<"mov $0x" << uint16_t(op2) << uint16_t(op1) << ", %dx" <<'\n';
-            ip += 3;
-            return 1;
-        default:
-            std::cout << uint16_t(op) << " " << uint16_t(op1) << " " << uint16_t(op2) << '\n';
-            return 0;
+    std::cout << uint16_t(op) << " " << uint16_t(op1) << " " << uint16_t(op2);
+    uint8_t argSize = 32;
+    uint16_t segment = ds;
+    std::array<uint16_t*, 8> reg16{&ax, &cx, &dx, &bx, &sp, &bp, &si, &di};
+    const std::array<std::string, 8> reg16Names{"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
+    std::array<segment_t*, 4> seg{&es, &cs, &ss, &ds};
+    std::array<std::string, 4> segNames{"es", "cs", "ss", "ds"};
+    while (true) {
+        switch(op) {
+            case 0x26: // ES sement override    00100
+            case 0x2e: // CS segment override   00101
+            case 0x36: // SS segment override   00110
+            case 0x3e: // DS segment override   00111
+                segment = *seg[(op>>3)&3];
+                ip++;
+                op = mem[getOffset(cs, ip)];
+                break;
+            case 0xb8: // ax
+            case 0xb9: // cx
+            case 0xba: // mov $0x1234, %dx
+            case 0xbb: // bx
+            case 0xbc: // sp
+            case 0xbd: // bp
+            case 0xbe: // si
+            case 0xbf: // di
+                *reg16[op&0x7] = op2 << 8 | op1;
+                std::cout << "\tmov $0x" << uint16_t(op2) << uint16_t(op1) << ", %" << reg16Names[op&0x07] << '\n';
+                ip += 3;
+                return 1;
+            default:
+                std::cout << '\n';
+                return 0;
+        }
     }
 }
 
@@ -31,4 +58,4 @@ void cpu::write16(size_t offset, uint16_t value) {
     uint8_t byte2 = value & 0xff;
     mem[offset] = byte2;
     mem[offset + 1] = byte1;
-} 
+}
--- cpu.h
+++ cpu.h
@@ -9,9 +9,13 @@ public:
     uint64_t step();
 private:
     uint16_t ax;
-    uint16_t bx;
     uint16_t cx;
     uint16_t dx;
+    uint16_t bx;
+    uint16_t sp;
+    uint16_t bp;
+    uint16_t si;
+    uint16_t di;
 
     uint16_t ip;
 
@@ -58,3 +62,101 @@ private:
     0x8000   MD mode (reserved); always 0
     */
 };
+
+/*
+one opcode format:
+0aaaaads
+aaaaa: opcode
+d: direction, 0=register to memory, 1=memory to register
+s: 0 for 8-bit operands, 1 for 16 or 32-bit operands (32 is default, 16 takes a prefix byte)
+
+1aaaaaxs
+aaaaa: opcode
+x: 0 indicates const is same size as specified by `s` field, 1 is 8-bit sign-extended
+s: 0 for 8-bit, 1 for 32/16-bit
+*/
+
+/*
+mod r/m byte:
+aabbbccc
+
+aa: MOD
+00: Register indirect addressing mode or SIB with no displacement (when R/M = 100) or Displacement only addressing mode (when R/M = 101). 
+01: One-byte signed displacement follows addressing mode byte(s).
+10: Four-byte signed displacement follows addressing mode byte(s).
+11: Register addressing mode.
+
+bbb: reg    8-bit 16-bit 32-bit
+000         al    ax     eax
+001         cl    cx     ecx
+010         dl    dx     edx
+011         bl    bx     ebx
+100         ah    sp     esp
+101         ch    bp     ebp
+110         dh    si     esi
+111         bh    di     edi
+
+r/m: aa ccc: 
+     00 000 [ eax ]
+     01 000 [ eax + disp8 ]               (1)
+     10 000 [ eax + disp32 ]
+     11 000 register  ( al / ax / eax )   (2)
+     00 001 [ ecx ]
+     01 001 [ ecx + disp8 ]
+     10 001 [ ecx + disp32 ]
+     11 001 register  ( cl / cx / ecx )
+     00 010 [ edx ]
+     01 010 [ edx + disp8 ]
+     10 010 [ edx + disp32 ]
+     11 010 register  ( dl / dx / edx )
+     00 011 [ ebx ]
+     01 011 [ ebx + disp8 ]
+     10 011 [ ebx + disp32 ]
+     11 011 register  ( bl / bx / ebx )
+     00 100 SIB  Mode                     (3)
+     01 100 SIB  +  disp8  Mode
+     10 100 SIB  +  disp32  Mode
+     11 100 register  ( ah / sp / esp )
+     00 101 32-bit Displacement-Only Mode (4)
+     01 101 [ ebp + disp8 ]
+     10 101 [ ebp + disp32 ]
+     11 101 register  ( ch / bp / ebp )
+     00 110 [ esi ]
+     01 110 [ esi + disp8 ]
+     10 110 [ esi + disp32 ]
+     11 110 register  ( dh / si / esi )
+     00 111 [ edi ]
+     01 111 [ edi + disp8 ]
+     10 111 [ edi + disp32 ]
+     11 111 register  ( bh / di / edi )
+
+SIB = scaled index byte
+ssiiibbb
+
+ss:
+00  index * 1
+01  index * 2
+10  index * 4
+11  index * 8
+
+iii:
+000	EAX
+001	ECX
+010	EDX
+011	EBX
+100	Illegal
+101	EBP
+110	ESI
+111	EDI
+
+bbb:
+000	xx	EAX
+001	xx	ECX
+010	xx	EDX
+011	xx	EBX
+100	xx	ESP
+101	00	Displacement-only
+101 01, 10	EBP
+110	xx	ESI
+111	xx	EDI 
+*/
